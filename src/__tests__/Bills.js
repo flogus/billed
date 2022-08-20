60bc7a446341c40a580a95d77707ddbe8301ff0e
flogus <fg@motionery.com>
2022-08-20 08:31:22 +0200

    add missing files

--- src/__tests__/Bills.js
+++ src/__tests__/Bills.js
@@ -2,38 +2,43 @@
  * @jest-environment jsdom
  */
 
-import {screen, waitFor} from "@testing-library/dom"
+import { screen, waitFor } from "@testing-library/dom"
 import BillsUI from "../views/BillsUI.js"
 import { bills } from "../fixtures/bills.js"
-import { ROUTES_PATH} from "../constants/routes.js";
-import {localStorageMock} from "../__mocks__/localStorage.js";
+import { ROUTES_PATH } from "../constants/routes.js";
+import { localStorageMock } from "../__mocks__/localStorage.js";
 
 import router from "../app/Router.js";
 
 describe("Given I am connected as an employee", () => {
-  describe("When I am on Bills Page", () => {
-    test("Then bill icon in vertical layout should be highlighted", async () => {
-
-      Object.defineProperty(window, 'localStorage', { value: localStorageMock })
-      window.localStorage.setItem('user', JSON.stringify({
-        type: 'Employee'
-      }))
-      const root = document.createElement("div")
-      root.setAttribute("id", "root")
-      document.body.append(root)
-      router()
-      window.onNavigate(ROUTES_PATH.Bills)
-      await waitFor(() => screen.getByTestId('icon-window'))
-      const windowIcon = screen.getByTestId('icon-window')
-      //to-do write expect expression
-
+    describe("When I am on Bills Page", () => {
+        test("Then I click on the new bill button", async() => {
+            const mockCallBack = jest.fn();
+            const button = render("<Button onClick=" + { mockCallBack } + ">Nouvelle note de frais</Button>");
+            button.find('button').simulate('click');
+            expect(mockCallBack.mock.calls.length).toEqual(1);
+        })
+        test("Then bill icon in vertical layout should be highlighted", async() => {
+            Object.defineProperty(window, 'localStorage', { value: localStorageMock })
+            window.localStorage.setItem('user', JSON.stringify({
+                type: 'Employee'
+            }))
+            const root = document.createElement("div")
+            root.setAttribute("id", "root")
+            document.body.append(root)
+            router()
+            window.onNavigate(ROUTES_PATH.Bills)
+            await waitFor(() => screen.getByTestId('icon-window'))
+            const windowIcon = screen.getByTestId('icon-window')
+            const windowIcon2 = getByTestId('icon-window')
+            expect(windowIcon2).toHaveClass('active-icon')
+        })
+        test("Then bills should be ordered from earliest to latest", () => {
+            document.body.innerHTML = BillsUI({ data: bills })
+            const dates = screen.getAllByText(/^(19|20)\d\d[- /.](0[1-9]|1[012])[- /.](0[1-9]|[12][0-9]|3[01])$/i).map(a => a.innerHTML)
+            const antiChrono = (a, b) => (a > b) ? 1 : (a > b ? -1 : 0)
+            const datesSorted = [...dates].sort(antiChrono)
+            expect(dates).toEqual(datesSorted)
+        })
     })
-    test("Then bills should be ordered from earliest to latest", () => {
-      document.body.innerHTML = BillsUI({ data: bills })
-      const dates = screen.getAllByText(/^(19|20)\d\d[- /.](0[1-9]|1[012])[- /.](0[1-9]|[12][0-9]|3[01])$/i).map(a => a.innerHTML)
-      const antiChrono = (a, b) => ((a < b) ? 1 : -1)
-      const datesSorted = [...dates].sort(antiChrono)
-      expect(dates).toEqual(datesSorted)
-    })
-  })
 })
--- src/__tests__/NewBill.js
+++ src/__tests__/NewBill.js
@@ -2,17 +2,22 @@
  * @jest-environment jsdom
  */
 
-import { screen } from "@testing-library/dom"
+import { getByTestId } from '@testing-library/dom'
+import { toHaveClass } from "@testing-library/dom"
 import NewBillUI from "../views/NewBillUI.js"
 import NewBill from "../containers/NewBill.js"
 
 
 describe("Given I am connected as an employee", () => {
-  describe("When I am on NewBill Page", () => {
-    test("Then ...", () => {
-      const html = NewBillUI()
-      document.body.innerHTML = html
-      //to-do write assertion
+    describe("When I am on NewBill Page", () => {
+        test("and the layout icon is active", () => {
+            // const html = NewBillUI()
+            const html = "<div id='layout-icon1' data-testid='icon-window' class='active-icon'></div>";
+
+            document.body.innerHTML = html
+            const component = document.getByTestId('icon-window');
+            console.log('component', component)
+            expect(component).toHaveClass('.active-icon');
+        })
     })
-  })
 })
